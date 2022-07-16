f363add7ed36853c58b72c35983022a4e201d030
Ndibechinonso <edwardndibe@gmail.com>
2022-07-16 15:18:46 +0100

    card fix

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./Card.css";
 
 export default function Card(props) {
+
+
   return (
     <div data-aos={props.anim} className="cardFlex">
       <div className="iconClass">
         <i className={props.link}></i>
       </div>
       <h2 style={{height: "70px"}}>{props.header}</h2>
-      <p style={{height: "157px"}}>{props.text}</p>
+      <p style={{height: `${props.portfolio ? "170px" : "70px"}`}}>{props.text}</p>
       <h3>{props.language}</h3>
-      <p style={{height: "70px"}}>{props.languages}</p>
+      <p style={{height: `${props.certification ? '100px': '70px'}`}}>{props.languages}</p>
 
       <h3>{props.tool}</h3>
       {props.tasks ? <div style={{height: "50px"}}>
@@ -21,11 +23,11 @@ export default function Card(props) {
           </div>
         );
       }) }</div> : null }
-      <div className="certDiv">
+     {props.certification &&  (<div className="certDiv">
         <h3>{props.certification}</h3>
-      </div>
+      </div>) }
       <div className="buttonRow">
-        {props.button ? <div>{props.button}</div>: null}
+        {props.button && (<div>{props.button}</div>)}
         <div>{props.button2}</div>
       </div>
     </div>
